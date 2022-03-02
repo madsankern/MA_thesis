0c4cd539f1f66b216259944e3293e12c6697841e
madsankern <jqk218@econ.ku.dk>
2022-03-02 09:28:01 +0100

    update

--- temp/last_period.py
+++ temp/last_period.py
@@ -6,15 +6,17 @@ from numba import njit, prange
 # Local module
 import utility
 
+# Need a name for housing in the last period (right now use d)
+
 # Define the objective function
 @njit
-def obj_last_period(d,x,par):
+def obj_last_period(h,x,par):
     """ objective function in last period """
     
-    # implied consumption (rest)
-    c = x-d
+    # implied consumption after choosing housing
+    c = x - par.p*h
 
-    return -utility.func(c,d,par) # Minus as the code minimizes the function
+    return -utility.func(c,h,par) # Minus as the code minimizes the function
 
 @njit(parallel=True)
 def solve(t,sol,par):
@@ -26,36 +28,36 @@ def solve(t,sol,par):
     c_keep = sol.c_keep[t]
     inv_v_adj = sol.inv_v_adj[t]
     inv_marg_u_adj = sol.inv_marg_u_adj[t]
-    d_adj = sol.d_adj[t]
+    h_adj = sol.h_adj[t]
     c_adj = sol.c_adj[t]
 
-    # a. keep
-    for i_p in prange(par.Np): # Permanent income state
-        for i_n in range(par.Nn): # Values of the durable
+    # a. keep (RENAME THIS)
+    for i_y in prange(par.Ny): # Income states
+        for i_h in range(par.Nh): # Values of the durable
             for i_m in range(par.Nm): # Exogeneous cash on hand grid
 
                 # i. states
-                n = par.grid_n[i_n]
+                h = par.grid_n[i_h]
                 m = par.grid_m[i_m]
 
                 # Check why this step is required
                 if m == 0: # forced c = 0 
-                    c_keep[i_p,i_n,i_m] = 0
-                    inv_v_keep[i_p,i_n,i_m] = 0
-                    inv_marg_u_keep[i_p,i_n,i_m] = 0
+                    c_keep[i_y,i_h,i_m] = 0
+                    inv_v_keep[i_y,i_h,i_m] = 0
+                    inv_marg_u_keep[i_y,i_h,i_m] = 0
                     continue
                 
                 # ii. optimal choice
-                c_keep[i_p,i_n,i_m] = m
+                c_keep[i_y,i_h,i_m] = m
 
                 # iii. optimal value
-                v_keep = utility.func(c_keep[i_p,i_n,i_m],n,par) # Utility of choice
-                inv_v_keep[i_p,i_n,i_m] = -1.0/v_keep # Compute minus inverse for numerical stability 
-                inv_marg_u_keep[i_p,i_n,i_m] = 1.0/utility.marg_func(c_keep[i_p,i_n,i_m],n,par) # Inverse of the marginal utility
+                v_keep = utility.func(c_keep[i_y,i_h,i_m],h,par) # Utility of choice
+                inv_v_keep[i_y,i_h,i_m] = -1.0/v_keep # Compute minus inverse for numerical stability 
+                inv_marg_u_keep[i_y,i_h,i_m] = 1.0/utility.marg_func(c_keep[i_y,i_h,i_m],h,par) # Inverse of the marginal utility
 
     # Solve adjuster problem
     # b. adj
-    for i_p in prange(par.Np): # Permanent income states
+    for i_y in prange(par.Ny): # Income states
         for i_x in range(par.Nx): # Cash on hand states
             
             # i. states
@@ -63,23 +65,19 @@ def solve(t,sol,par):
 
             # Cannot do anything here
             if x == 0: # forced c = d = 0
-                d_adj[i_p,i_x] = 0
-                c_adj[i_p,i_x] = 0
-                inv_v_adj[i_p,i_x] = 0
-                inv_marg_u_adj[i_p,i_x] = 0
+                h_adj[i_y,i_x] = 0
+                c_adj[i_y,i_x] = 0
+                inv_v_adj[i_y,i_x] = 0
+                inv_marg_u_adj[i_y,i_x] = 0
                 continue
-
-            # ii. optimal choices
-            # d_low = 0 # Minimum value of housing
-            # d_high = np.min([x, par.n_max])  #np.fmin(x,par.n_max) # Maximum available to purchase
             
-            d_allow = par.d_grid[par.d_grid <= x] # values of housing that one can afford given x
-            objective = obj_last_period(par.allow, x, par) # utility values on grid of allowed housing
-            d_adj[i_p,i_x] = np.argmax(objective) # Find optimal value of housing
-            c_adj[i_p,i_x] = x - d_adj[i_p,i_x] # Find implied consumption choice (using all funds left)
+            h_allow = par.grid_h[par.grid_h <= x / par.p] # values of housing that one can afford given x
+            objective = obj_last_period(h, x, par) # utility values on grid of allowed housing
+            h_adj[i_y,i_x] = np.argmax(objective) # Find optimal value of housing
+            c_adj[i_y,i_x] = x - h_adj[i_y,i_x] # Find implied consumption choice (using all funds left)
             # Just add house prices, then it should be fine
 
             # iii. optimal value
-            v_adj = -obj_last_period(d_adj[i_p,i_x],x,par)
-            inv_v_adj[i_p,i_x] = -1.0/v_adj
-            inv_marg_u_adj[i_p,i_x] = 1.0/utility.marg_func(c_adj[i_p,i_x],d_adj[i_p,i_x],par)
+            v_adj = -obj_last_period(h_adj[i_y,i_x],x,par)
+            inv_v_adj[i_y,i_x] = -1.0/v_adj
+            inv_marg_u_adj[i_y,i_x] = 1.0/utility.marg_func(c_adj[i_y,i_x],h_adj[i_y,i_x],par)
