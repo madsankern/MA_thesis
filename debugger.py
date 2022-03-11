56ede4c270be1f91dde60321a927766739b8f4d9
madsankern <jqk218@econ.ku.dk>
2022-03-11 15:52:38 +0100

    Upload

---
+++ debugger.py
@@ -0,0 +1,20 @@
+# For debugging
+
+import time
+import numpy as np
+import numba as nb
+import matplotlib.pyplot as plt
+
+# load the DurableConsumptionModel module
+from DurableConsumptionModel import DurableConsumptionModelClass
+
+# Set the number of threads in numba
+nb.set_num_threads(4)
+
+# Setup model
+T = 7
+model = DurableConsumptionModelClass(name='example_negm',par={'solmethod':'negm','T':T,'do_print':True})
+
+# Run model
+# model.precompile_numba()
+model.solve()
