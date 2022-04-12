d39467c219daa753faada5c337eca48ca2195480
madsankern <jqk218@econ.ku.dk>
2022-04-12 09:20:22 +0200

    Bug fix

--- path.py
+++ path.py
@@ -3,7 +3,7 @@ import numpy as np
 def gen_path_R(par):
 
     # Permanent drop
-    par.path_R[:] = par.R - 0.01 # change the drop
+    par.path_R[par.sim_T:] = par.R - 0.01 # change the drop
     
     # Add other types of paths
     # Permanent increase, AR process
