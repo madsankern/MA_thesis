8b175bd3cc7a3135347b7cd96f31151428c3077e
madsankern <jqk218@econ.ku.dk>
2022-05-09 15:17:08 +0200

    Updated variable ordering

--- trans.py
+++ trans.py
@@ -11,12 +11,11 @@ def n_plus_func(d,par):
 
 # b. Cash on hand when keeping
 @njit(fastmath=True)
-def m_plus_func(a,y_plus,par,n,R,ph):
+def m_plus_func(a,R,y_plus,n,ph,par):
     m_plus = R*a + y_plus - par.deltaa*n - par.tauc*ph*n
     return m_plus
 
 # c. Cash on hand when adjusting
 @njit(fastmath=True)
-def x_plus_func(m_plus,n_plus,pb,par,ph):
-    return m_plus + (ph*n_plus - (1.0-par.phi)*pb*n_plus)  - par.taug*n_plus*(pb - ph)
-
+def x_plus_func(m_plus,n_plus,ph,pb,par):
+    return m_plus + (ph*n_plus - (1.0-par.phi)*pb*n_plus) - par.taug*n_plus*(ph - pb) # check if this last term really make senses
