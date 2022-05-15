89fd6fc008c493ee463bbdd396eaf6c016fe2fe3
madsankern <jqk218@econ.ku.dk>
2022-05-15 09:42:09 +0200

    Initial commit

---
+++ funcs.py
@@ -0,0 +1,29 @@
+# ----------------------------- #
+# Collection of functions I use #
+# ----------------------------- #
+import numpy as np
+from numba import njit
+
+# @njit
+def gini(x, w=None):
+
+    '''Compute the gini coefficient for array x'''
+
+    # The rest of the code requires numpy arrays.
+    x = np.asarray(x)
+    if w is not None:
+        w = np.asarray(w)
+        sorted_indices = np.argsort(x)
+        sorted_x = x[sorted_indices]
+        sorted_w = w[sorted_indices]
+        # Force float dtype to avoid overflows
+        cumw = np.cumsum(sorted_w, dtype=float)
+        cumxw = np.cumsum(sorted_x * sorted_w, dtype=float)
+        return (np.sum(cumxw[1:] * cumw[:-1] - cumxw[:-1] * cumw[1:]) / 
+                (cumxw[-1] * cumw[-1]))
+    else:
+        sorted_x = np.sort(x)
+        n = len(x)
+        cumx = np.cumsum(sorted_x, dtype=float)
+        # The above formula, with all weights equal to 1 simplifies to:
+        return (n + 1 - 2 * np.sum(cumx) / cumx[-1]) / n
