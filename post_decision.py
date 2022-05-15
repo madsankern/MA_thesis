8e3922c179d90417d831b1fd213516d745fb4957
madsankern <jqk218@econ.ku.dk>
2022-05-15 09:42:39 +0200

    Update

--- post_decision.py
+++ post_decision.py
@@ -35,7 +35,7 @@ def compute_wq(t,R,sol,par,ph,compute_q=False):
             inv_marg_u_adj_plus = np.zeros(par.Na)
             
             # loop over other outer post-decision states
-            for i_n in prange(par.Nn): # think of this as the post decision housing
+            for i_n in range(par.Nn): # think of this as the post decision housing
 
                 # a. Income and and housing state
                 y = par.grid_y[i_y]
@@ -54,8 +54,8 @@ def compute_wq(t,R,sol,par,ph,compute_q=False):
                     n_plus = trans.n_plus_func(n,par)
 
                     # iii. Prepare interpolators
-                    prep_keep = linear_interp.interp_1d_prep(par.Na) #linear_interp.interp_2d_prep(par.grid_n,n_plus,par.Na)
-                    prep_adj = linear_interp.interp_1d_prep(par.Na)
+                    # prep_keep = linear_interp.interp_1d_prep(par.Na) #linear_interp.interp_2d_prep(par.grid_n,n_plus,par.Na)
+                    # prep_adj = linear_interp.interp_1d_prep(par.Na)
 
                     # iv. Weight of each income shock from Markov probabilities
                     weight = par.p_mat[i_y,ishock]
@@ -70,12 +70,22 @@ def compute_wq(t,R,sol,par,ph,compute_q=False):
                         x_plus[i_a] = trans.x_plus_func(m_plus[i_a],n_plus,ph,pb,par) # should this be pb in t+1?
                     
                     # vi. Interpolate
-                    linear_interp.interp_1d_vec_mon(prep_keep,par.grid_m,sol.inv_v_keep[t+1,i_pb,ishock,i_n],m_plus,inv_v_keep_plus)
-                    linear_interp.interp_1d_vec_mon(prep_adj,par.grid_x,sol.inv_v_adj[t+1,i_pb,ishock],x_plus,inv_v_adj_plus)
+                    # linear_interp.interp_1d_vec_mon(prep_keep,par.grid_m,sol.inv_v_keep[t+1,i_pb,ishock,i_n],m_plus,inv_v_keep_plus)
+                    # linear_interp.interp_1d_vec_mon(prep_adj,par.grid_x,sol.inv_v_adj[t+1,i_pb,ishock],x_plus,inv_v_adj_plus)
                     
-                    linear_interp.interp_1d_vec_mon_rep(prep_keep,par.grid_m,sol.inv_marg_u_keep[t+1,i_pb,ishock,i_n],m_plus,inv_marg_u_keep_plus)
-                    linear_interp.interp_1d_vec_mon_rep(prep_adj,par.grid_x,sol.inv_marg_u_adj[t+1,i_pb,ishock],x_plus,inv_marg_u_adj_plus)
-                        
+                    # linear_interp.interp_1d_vec_mon_rep(prep_keep,par.grid_m,sol.inv_marg_u_keep[t+1,i_pb,ishock,i_n],m_plus,inv_marg_u_keep_plus)
+                    # linear_interp.interp_1d_vec_mon_rep(prep_adj,par.grid_x,sol.inv_marg_u_adj[t+1,i_pb,ishock],x_plus,inv_marg_u_adj_plus)
+
+                    # vi_prime Test interpolate
+                    linear_interp.interp_1d_vec(par.grid_m,sol.inv_v_keep[t+1,i_pb,ishock,i_n],m_plus,inv_v_keep_plus)
+                    linear_interp.interp_1d_vec(par.grid_x,sol.inv_v_adj[t+1,i_pb,ishock],x_plus,inv_v_adj_plus)
+
+                    linear_interp.interp_1d_vec(par.grid_m,sol.inv_marg_u_keep[t+1,i_pb,ishock,i_n],m_plus,inv_marg_u_keep_plus)
+                    linear_interp.interp_1d_vec(par.grid_x,sol.inv_marg_u_adj[t+1,i_pb,ishock],x_plus,inv_marg_u_adj_plus)
+                    
+                    # assert np.all(inv_marg_u_keep_plus >= 0)
+                    # assert np.all(inv_marg_u_adj_plus >= 0)
+                    
                     # vii. Find max and accumulate.
                     for i_a in range(par.Na):
                         
@@ -96,6 +106,6 @@ def compute_wq(t,R,sol,par,ph,compute_q=False):
                         w[i_a] += weight*par.beta*v_plus
                         q[i_pb,i_y,i_n,i_a] += weight*par.beta*R*marg_u_plus
             
-                # d. transform post decision value function
+                # d. Transform post decision value function
                 for i_a in range(par.Na):
-                    inv_w[i_pb,i_y,i_n,i_a] = -1/w[i_a] # Transform to inverse for computations
+                    inv_w[i_pb,i_y,i_n,i_a] = -1/w[i_a]
