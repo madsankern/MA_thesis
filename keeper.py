154f9ace677c578b6137380ba28d8c1a98b27c41
madsankern <jqk218@econ.ku.dk>
2022-05-27 09:36:02 +0200

    Updated documentation

--- keeper.py
+++ keeper.py
@@ -36,7 +36,7 @@ def solve_keep(t,sol,par):
                 n = par.grid_n[i_n]
                 for i_a in range(par.Na):
                     q_c[i_pb,i_y,i_n,i_a] = utility.inv_marg_func(sol.q[t,i_pb,i_y,i_n,i_a],n,par)
-                    q_m[i_pb,i_y,i_n,i_a] = par.grid_a[i_a] + q_c[i_pb,i_y,i_n,i_a]
+                    q_m[i_pb,i_y,i_n,i_a] = par.grid_a[i_a] + q_c[i_pb,i_y,i_n,i_a] # check this
             
                 # ii. Apply upper envelope
                 negm_upperenvelope(par.grid_a,q_m[i_pb,i_y,i_n],q_c[i_pb,i_y,i_n],sol.inv_w[t,i_pb,i_y,i_n],
